68d0fa994b311879f495f4c4380b683e16da6722
Rudolf Ádám <sari.vogel.94@gmail.com>
2019-01-06 12:24:23 +0100

    Send horoscope-input in POST request

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ function addHoroscope() {
 
     var formData = new FormData();
     formData.append('birthday', document.getElementById('birthdayInput').value);
+    formData.append('horoscope-input', document.getElementById('horoscope-input').value);
 
     fetch("/addHoroscope.php", {
             method: 'POST',
@@ -14,7 +15,7 @@ function addHoroscope() {
             console.log(json);
             var horoscopeText = document.createElement('p');
             horoscopeText.innerText = json;
-            document.getElementById('horoscope-text').appendChild(horoscopeText);
+            document.getElementById('horoscope-output').appendChild(horoscopeText);
         })
         .catch((err) => {
             console.log(err);
