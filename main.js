9714a7b8e8875590fdf5052566a9989d21bd85a6
Rudolf Ádám <sari.vogel.94@gmail.com>
2019-01-07 21:11:45 +0100

    Send birthday to viewHoroscope.php

--- main.js
+++ main.js
@@ -1,10 +1,25 @@
 
 function viewHoroscope() {
-    
+
+    var birthday = document.getElementById('birthdayInput').value;
+
+    fetch("/viewHoroscope.php?birthday="+birthday, {
+            method: 'GET',
+            credentials: 'include'
+        })
+        .then((response) => response.json())
+        .then((json) => {
+            console.log(json);
+            var horoscopeText = document.createElement('p');
+            horoscopeText.innerText = json;
+            document.getElementById('horoscope-output').appendChild(horoscopeText);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
 }
 
 function addHoroscope() {
-
     var formData = new FormData();
     formData.append('birthday', document.getElementById('birthdayInput').value);
     formData.append('horoscope-input', document.getElementById('horoscope-input').value);
